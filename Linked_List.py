9d3adf36ad3e64b1d2cdbd1a04e57f3586caf06e
bhanuka1998 <52658763+bhanuka1998@users.noreply.github.com>
2019-09-11 10:23:59 +0530

    Implemented insert in middle function

--- Linked_List.py
+++ Linked_List.py
@@ -4,7 +4,7 @@ class Node:
         self.next_node = next_node
 
 
-class Linked_List:
+class LinkedList:
     def __init__(self, head=None, tail=None):
         self.head = head
         self.tail = tail
@@ -29,8 +29,28 @@ class Linked_List:
             print(current_node.data)
             current_node = current_node.next_node
 
+    def insert_end(self, new_node):
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
 
-list_1 = Linked_List()
+    def insert_middle(self, new_node, value):
+        current_node = self.head
+        #self.value = value
+        while current_node.data != value and current_node:
+            current_node = current_node.next_node
+        new_node.next_node = current_node.next_node
+        current_node.next_node = new_node
+
+
+list_1 = LinkedList()
 list_1.insert_front(Node(data=5))
 list_1.insert_front(Node(data=3))
+list_1.insert_end(Node(data=8))
+list_1.insert_middle(Node(data=4), 5)
+list_1.insert_middle(Node(data=7), 4)
+list_1.traverse()
 print(list_1.is_empty())
